87a62d83f601743b1e4af4dbc9c2ff3a02bf458b
Aleksei Nesterinen <aleksei.nesterinen@outlook.com>
2025-02-25 12:08:42 +0200

    file structure and fullcalendar plugin

--- AutoKalenteri.php
+++ AutoKalenteri.php
@@ -5,4 +5,19 @@
  * Version: 1.0
  * Author: Aleksei Nesterinen
  */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function write_log( $data ) {
+    if ( true === WP_DEBUG ) {
+        if ( is_array( $data ) || is_object( $data ) ) {
+            error_log( print_r( $data, true ) );
+        } else {
+            error_log( $data );
+        }
+    }
+}
+
 ?>
