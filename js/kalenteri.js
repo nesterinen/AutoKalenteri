748cda0ea5a15b122d76620b82e633ad1668e08b
Aleksei Nesterinen <aleksei.nesterinen@outlook.com>
2025-03-03 13:41:41 +0200

    added functionality to popup for changing reservation times

--- js/kalenteri.js
+++ js/kalenteri.js
@@ -20,16 +20,47 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     }
 
-    async function Popup() {
+    async function Popup(startTime, endTime) {
+        var startTimeVariable = dateNoTimezone(startTime).split("T")[1].split(".")[0].split(":")  // turn dateobj to string array [0]hours [1]minutes
+        var endTimeVariable = dateNoTimezone(endTime).split("T")[1].split(".")[0].split(":")  // turn dateobj to string array [0]hours [1]minutes
+
         return new Promise((resolve) => {
           const myDialog = document.createElement("dialog")
           myDialog.setAttribute('id', 'varausPopup')
           document.body.appendChild(myDialog)
         
           const header = document.createElement('h2')
-          var text = document.createTextNode("Auton varaus")
+          const text = document.createTextNode("Auton varaus")
           header.appendChild(text)
+
+
+          /*
+          const startTimeElement = document.createElement('p')
+          var startTimeElementParagraph = document.createTextNode("alku")
+          startTimeElement.appendChild(startTimeElementParagraph)
+          var startTimeElementInput = document.createElement('input')
+          */
+
+          const resrvTimes = document.createElement('div')
+          resrvTimes.setAttribute('id', "popupStartEndTimes")
+
+          const rasrvParagraph = document.createElement('p')
+          const rasrvParagraphText = document.createTextNode('klo')
+          rasrvParagraph.appendChild(rasrvParagraphText)
+
+          var startTextField = document.createElement('input')
+          startTextField.setAttribute('type', 'time')
+          startTextField.setAttribute('value', `${startTimeVariable[0]}:${startTimeVariable[1]}`)
+
+          var endTextField = document.createElement('input')
+          endTextField.setAttribute('type', 'time')
+          endTextField.setAttribute('value', `${endTimeVariable[0]}:${endTimeVariable[1]}`)
+
+          resrvTimes.appendChild(rasrvParagraph)
+          resrvTimes.appendChild(startTextField)
+          resrvTimes.appendChild(endTextField)
         
+         
           var closeButton = document.createElement('button')
           closeButton.textContent = 'peruuta'
           closeButton.setAttribute('id', 'closeButton')
@@ -55,9 +86,9 @@ document.addEventListener('DOMContentLoaded', async () => {
           select.appendChild(auto4)
 
           const varaajaText = document.createElement('p')
-          var text2 = document.createTextNode("Varaaja")
+          const text2 = document.createTextNode("Varaaja")
           varaajaText.appendChild(text2)
-          varaajaInput = document.createElement('input')
+          var varaajaInput = document.createElement('input')
         
           function dialogClose() {
             addButton.removeEventListener('click', () => dialogAdd())
@@ -69,12 +100,24 @@ document.addEventListener('DOMContentLoaded', async () => {
           function dialogAdd() {
             addButton.removeEventListener('click', () => dialogAdd())
             closeButton.removeEventListener('click', () => dialogClose())
+
+            const startDateObj = new Date(startTime)
+            startTimeVariable = startTextField.value.split(':')
+            startDateObj.setHours(startTimeVariable[0])
+            startDateObj.setMinutes(startTimeVariable[1])
+
+            const endDateObj = new Date(endTime)
+            endTimeVariable = endTextField.value.split(':')
+            endDateObj.setHours(endTimeVariable[0])
+            endDateObj.setMinutes(endTimeVariable[1])
+
             myDialog.close()
-            resolve({value: select.value, input: varaajaInput.value})
+            resolve({value: select.value, input: varaajaInput.value, start: startDateObj, end: endDateObj})
           }
           
           myDialog.appendChild(header)
           myDialog.appendChild(select)
+          myDialog.appendChild(resrvTimes)
           myDialog.appendChild(varaajaText)
           myDialog.appendChild(varaajaInput)
           myDialog.appendChild(addButton)
@@ -137,11 +180,13 @@ document.addEventListener('DOMContentLoaded', async () => {
         
         select: async function(arg) {
             //var title = prompt('Event Title:');
-            var popUpResult = await Popup()
+            var popUpResult = await Popup(arg.start, arg.end)
 
             if (popUpResult) {
             var title = popUpResult.value
             var varaaja = popUpResult.input
+            var reservationStartTime = popUpResult.start
+            var reservationEndTime = popUpResult.end
 
             jQuery.ajax({
                 type: "POST",
@@ -150,8 +195,8 @@ document.addEventListener('DOMContentLoaded', async () => {
                 data: {
                     action:'post_db',
                     title: title,
-                    start: dateNoTimezone(arg.start),
-                    end: dateNoTimezone(arg.end),
+                    start: dateNoTimezone(reservationStartTime),
+                    end: dateNoTimezone(reservationEndTime),
                     varaaja: varaaja
                 },
                 success: function(response){
@@ -159,8 +204,8 @@ document.addEventListener('DOMContentLoaded', async () => {
                     ID: response.data.id,
                     publicId: response.data.id,
                     title: title,
-                    start: arg.start,
-                    end: arg.end,
+                    start: reservationStartTime,
+                    end: reservationEndTime,
                     color: colorCase(title),
                     varaaja: varaaja
                     })
